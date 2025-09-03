3e90b7d8897408ec0ec0f3ba7abfb81fbee6f796
RicardoBarrote <ricardoambscfc@gmail.com>
2025-09-03 15:47:08 -0300

    [Class] AlbumWrapper

---
+++ src/main/java/br/com/spotify/client/AlbumWrapper.java
@@ -0,0 +1,23 @@
+package br.com.spotify.client;
+
+import java.util.List;
+
+public class AlbumWrapper {
+
+    private List<Album> items;
+
+    public AlbumWrapper() {
+    }
+
+    public AlbumWrapper(List<Album> items) {
+        this.items = items;
+    }
+
+    public List<Album> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Album> items) {
+        this.items = items;
+    }
+}
